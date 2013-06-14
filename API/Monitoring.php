b6bd902c55305aab2112a65a0ca084463885bfc5
Dominik Zogg <dominik.zogg@gmail.com>
2013-06-14 15:56:49 +0200

    enhance monitoring type

--- API/Monitoring.php
+++ API/Monitoring.php
@@ -20,7 +20,13 @@ class Monitoring extends AbstractType
         return call_user_func_array(array($this, 'createMonitoring'), func_get_args());
     }
 
-    public function createMonitoring($user_id, $date, $client_service_id, $duration, $allowable_bill = true, $tracking_type = 0)
+    public function createMonitoring($user_id, $contact_id, $client_service_id, $date, $duration, $allowable_bill = true, $tracking_type = 0, $monitoring_status_id = 1)
+    {
+        $vars = compact(array_keys(get_defined_vars()));
+        return $this->create($vars);
+    }
+
+    public function createFromToMonitoring($user_id, $contact_id, $client_service_id, $needed_hour_start, $needed_hour_end, $allowable_bill = true, $tracking_type = 1, $monitoring_status_id = 1)
     {
         $vars = compact(array_keys(get_defined_vars()));
         return $this->create($vars);
